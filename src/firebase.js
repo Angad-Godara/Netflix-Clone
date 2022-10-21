71c89f8e4c99c67f8c5a62bd049728af8dbc46a8
Angad-Godara <angadrrr2002@gmail.com>
2022-10-21 14:45:44 +0530

    ADD: starting netflix

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDyxjV1_MeGjdLGVDnLg33Xb5sReIPynTM",
+    authDomain: "netflix-clone-15644.firebaseapp.com",
+    projectId: "netflix-clone-15644",
+    storageBucket: "netflix-clone-15644.appspot.com",
+    messagingSenderId: "136895104885",
+    appId: "1:136895104885:web:aa161074a7fafe1551f53c"
+};
