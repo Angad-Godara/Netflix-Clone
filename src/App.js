e4c18f622f089a4158bbef7c9cafc945e933541b
Angad-Godara <angadrrr2002@gmail.com>
2023-01-02 23:59:37 +0530

    RES: resuming the project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import './App.css';
 import HomeScreen from './Components/HomeScreen';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import LoginScreen from './Components/LoginScreen'
 
 function App() {
+
+  const user = null;
+
   return (
     <div className="app">
-      <HomeScreen />
+      <Router>
+        {!user ?
+          <LoginScreen />
+          : (
+            <Routes>
+              <Route exact path="/"
+                element={<HomeScreen />}
+              />
+            </Routes>
+          )}
+      </Router>
     </div>
   );
 }
